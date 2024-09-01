e306661d8840d884cc69ff4bcf3a0f3b287f5277
Fahim-Chowdury <fvai7679@gmail.com>
2024-09-01 21:18:26 +0600

    Update animevideo.js

--- scripts/commands/animevideo.js
+++ scripts/commands/animevideo.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "anime",
   version: "1.0.0",
   permission: 0,
-  credits: "Farhan 𝖠𝗋𝖸𝖠𝖭",
+  credits: "Fahim",
   description: "Random sad video",
   prefix: true,
   category: "Media",
@@ -20,7 +20,7 @@ module.exports.run = async ({ api, event, args, client, Users, Threads, __GLOBAL
   const request = global.nodemodule["request"];
   const fs = global.nodemodule["fs-extra"];
 
-  var hi = ["♥︎╣[-𝖠 𝖱 𝖸 𝖠 𝖭-]╠♥︎"];
+  var hi = ["𝐅𝐀𝐇𝐈𝐌 𝐂𝐇𝐎𝐖𝐃𝐇𝐔𝐑𝐘"];
   var know = hi[Math.floor(Math.random() * hi.length)];
   var link = [
     "https://drive.google.com/uc?id=18-qJqj0yJOe1DnqtKCtt2BA6aL4Lsu1V",
