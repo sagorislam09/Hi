30f02bc5a03f88e65c347381fbfc2b7ac2faf73b
Fahim-Chowdury <fvai7679@gmail.com>
2024-09-02 13:08:47 +0600

    Update egdat.js

--- scripts/commands/egdat.js
+++ scripts/commands/egdat.js
@@ -16,7 +16,7 @@ handleEvent: function({ api, event, client, __GLOBAL }) {
   var { threadID, messageID } = event;
   const content = event.body ? event.body : '';
   const body = content.toLowerCase();
-  if (body.indexOf("😃")==0 || body.indexOf("😄")==0 || body.indexOf("😆")==0 || body.indexOf("😆")==0) {
+  if (body.indexOf("😁")==0 || body.indexOf("😁")==0 || body.indexOf("😁")==0 || body.indexOf("😁")==0) {
     var msg = {
         body: "~ তোমার দাঁত গুলো চক চক করছে গো..🙉🥀",
         attachment: fs.createReadStream(__dirname + `/Nayan/datcokcok.mp3`)
