4e30bf889879b5308377ea26ce53ba208d77567a
Fahim-Chowdury <fvai7679@gmail.com>
2024-09-02 13:31:27 +0600

    Update egjibba.js

--- scripts/commands/egjibba.js
+++ scripts/commands/egjibba.js
@@ -16,7 +16,7 @@ handleEvent: function({ api, event, client, __GLOBAL }) {
   var { threadID, messageID } = event;
   const content = event.body ? event.body : '';
   const body = content.toLowerCase();
-  if (body.indexOf("😋")==0 || body.indexOf("😜")==0 || body.indexOf("🤪")==0 || body.indexOf("😝")==0) {
+  if (body.indexOf("😛")==0 || body.indexOf("😝")==0 || body.indexOf("🤪")==0 || body.indexOf("😝")==0) {
     var msg = {
         body: "জি্ঁব্বা্ঁ দে্ঁখা্ঁও্ঁ কে্ঁটে্ঁ দি্ঁমু্ঁ 😒🥀",
         attachment: fs.createReadStream(__dirname + `/Nayan/jibba.mp3`)
