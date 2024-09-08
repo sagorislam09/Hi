f08583c05c2f5a27a5f6b7448f3343f380ae2fd6
ARYAN CHOWDHURY <aryanchowdhury439@yahoo.com>
2024-09-08 19:45:41 +0600

    Update convert.js

--- scripts/commands/convert.js
+++ scripts/commands/convert.js
@@ -19,7 +19,7 @@ var audioss = []
     var { data } = await axios.get(audio ,{  method: 'GET',  responseType: 'arraybuffer' });
                 fs.writeFileSync(__dirname + "/cache/vdtoau.m4a", Buffer.from(data, 'utf-8'));
   audioss.push(fs.createReadStream(__dirname + "/cache/vdtoau.m4a"));
-    var msg = { body : "𝙲𝙾𝙽𝚅𝙴𝚁𝚃 𝙵𝙰𝙷𝙸𝙼 𝙼𝙿𝟹 𝚂𝙾𝙽𝙶 ️🎶\n𝐅𝐀𝐇𝐈𝐌 𝐂𝐇𝐎𝐖𝐃𝐇𝐔𝐑𝐘", attachment: audioss}
+    var msg = { body : "𝙲𝙾𝙽𝚅𝙴𝚁𝚃 𝙰𝚁𝚈𝙰𝙽 𝙼𝙿𝟹 𝚂𝙾𝙽𝙶 ️🎶\n𝙰𝚁𝚈𝙰𝙽 𝙼𝙸𝙳𝙴𝙰", attachment: audioss}
   api.sendMessage(msg, event.threadID, event.messageID)
 } catch(e){
     console.log(e)
