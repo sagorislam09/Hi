23347f537c883dacfd700d35cd49f50a2e469450
ARYAN CHOWDHURY <aryanchowdhury439@yahoo.com>
2024-09-08 20:08:02 +0600

    Update hentaivideo.js

--- scripts/commands/hentaivideo.js
+++ scripts/commands/hentaivideo.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 const fs = require('fs');
 
 module.exports.config = {
-    name: "hentaivideo", 
+    name: "hvideo", 
     version: "1.0.0", 
     permssion: 0, 
     premium: false,
@@ -31,7 +31,7 @@ module.exports.run = async ({ api, event, args }) => {
         response.data.pipe(writer);
 
         writer.on('finish', () => {
-            api.sendMessage({ body: "♻️--『𝐅𝐀𝐇𝐈𝐌 𝐂𝐇𝐎𝐖𝐃𝐇𝐔𝐑𝐘』--♻️", attachment: fs.createReadStream(videoPath) }, event.threadID);
+            api.sendMessage({ body: "♻️--『𝗔𝗥𝗬𝗔𝗡 𝗖𝗛𝗢𝗪𝗗𝗛𝗨𝗥𝗬』--♻️", attachment: fs.createReadStream(videoPath) }, event.threadID);
         });
 
         writer.on('error', (err) => {
